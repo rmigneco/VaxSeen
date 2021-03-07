7be842198e69b5bb316f0b03431e92911073849b
Ray Migneco <ray.migneco@gmail.com>
2021-03-06 20:56:50 -0500

    Initial Commit

---
+++ VaxSeen/VaxSeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaxSeenApp.swift
+//  VaxSeen
+//
+//  Created by Ray Migneco on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct VaxSeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
