2d02a4121f25986c0ef57aa143831f843a235cbf
Ray Migneco <ray.migneco@gmail.com>
2021-03-06 21:45:51 -0500

    Wrapped up in navigation views

--- VaxSeen/ContentView.swift
+++ VaxSeen/ContentView.swift
@@ -8,20 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedCVS = false
+    
     var body: some View {
-        VStack(alignment: .center, spacing: nil, content: {
-            Text("Welcome to VaxSeen!")
-                .padding()
-            Text("Choose a pharmacy below to check for appointments")
-                .multilineTextAlignment(.center)
-                .padding()
-            Button("CVS") {
-                //push on to CVS
-            }
-            .frame(width: 100, height: 60, alignment: .center)
-            .border(Color.red, width: 5)
-            .cornerRadius(5)
-        })
+        NavigationView {
+            VStack(alignment: .center, spacing: nil, content: {
+                Text("Welcome to VaxSeen!")
+                    .font(.title)
+                    .padding()
+                Text("Choose a pharmacy below to check for appointments")
+                    .multilineTextAlignment(.center)
+                    .padding()
+                NavigationLink(destination: CVSView()) {
+                    Text("CVS")
+                        .frame(width: 100, height: 60, alignment: .center)
+                        .border(Color.red, width: 5)
+                        .cornerRadius(5)
+                }
+            })
+        }
     }
 }
 
