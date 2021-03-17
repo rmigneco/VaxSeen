c31d6bf4275052777bbc6dbfa908dcda00631152
Ray Migneco <ray.migneco@gmail.com>
2021-03-17 09:03:23 -0400

    Added RiteAidStoreListView

---
+++ VaxSeen/Views/RiteAidStoreListView.swift
@@ -0,0 +1,21 @@
+//
+//  RiteAidStoreListView.swift
+//  VaxSeen
+//
+//  Created by Ray Migneco on 3/17/21.
+//
+
+import SwiftUI
+
+struct RiteAidStoreListView: View {
+    
+    var body: some View {
+        Text("Checking Rite Aid....")
+    }
+}
+
+struct RiteAidStoreListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RiteAidStoreListView()
+    }
+}
